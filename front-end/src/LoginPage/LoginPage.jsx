2294fd243c8f06ba393a0d1e93cbebfb156ec241
Tiago Citrangulo da Silva <tiagocitrangulo256@gmail.com>
2026-01-19 23:58:13 -0300

    feat: cria uma página de login e adiciona um sistema de rotas

---
+++ front-end/src/LoginPage/LoginPage.jsx
@@ -0,0 +1,34 @@
+
+import "./LoginPage.css"
+import React, { useState } from 'react'
+
+function LoginPage() {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault()
+        console.log(username)
+        console.log(password)
+
+    }
+
+    return (
+        <div id="loginContainer">
+            <div id="loginForm">
+                <h1>Login</h1>
+                <form autoComplete="off" onSubmit={handleSubmit}>
+                    <p><label htmlFor="username">Nome de usuário: </label><input type="text" name="" id="username" placeholder="Nome de usuário" required onChange={(e) => setUsername(e.target.value)} /></p>
+                    <p><label htmlFor="password">Senha: </label><input type="password" name="" id="password" placeholder="Senha" required onChange={(e) => setPassword(e.target.value)} /></p>
+                    <input type="submit" value="Entrar" id="btnLogin" />
+                    <p id="msgCadastro">Não tem conta? <a href="">Cadastre-se</a></p>
+                </form>
+            </div>
+        </div>
+    )
+
+}
+
+export default LoginPage;
