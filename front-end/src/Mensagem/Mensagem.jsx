dc818e0f8bc7af8da8d5a357fdd182c156c5ee8b
Tiago Citrangulo da Silva <tiagocitrangulo256@gmail.com>
2026-01-24 03:02:09 -0300

    feat: adiciona uma mensagem de falha
    - Adiciona a possibilidade de escolher, no componente Mensagem, mostrar uma mensagem de sucesso de operação (verde) ou uma mensagem de falha (vermelha)

--- front-end/src/Mensagem/Mensagem.jsx
+++ front-end/src/Mensagem/Mensagem.jsx
@@ -1,27 +1,27 @@
 
-import "./Mensagem.css"
 import { useEffect, useState } from "react";
+import "./Mensagem.css";
 
-function Mensagem({texto, visivel, estado}){
+function Mensagem({ texto, visivel, estado, falha = false }) {
 
     const [mostrada, setMostrada] = useState(false)
 
     useEffect(() => {
 
-        if(visivel == true && !mostrada){
+        if (visivel == true && !mostrada) {
             setMostrada(true)
             setTimeout(() => {
                 estado(true)
             }, 5000)
-        }else{
+        } else {
             setMostrada(false)
         }
 
     }, [visivel])
 
 
-    return(
-        <div className={visivel ? "mensagem" : "mensagem hidden"}>
+    return (
+        <div className={`mensagem ${(!visivel && "hidden")} ${(falha) ? "falha" : "sucesso"}`}>
             <p>{texto}</p>
         </div>
     )
